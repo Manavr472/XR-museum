6ae2e7bea41d877111d08733fb6d52b432396d97
Manavr472 <manavr472@gmail.com>
2025-02-27 22:04:18 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2179145.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 215729478, 215729479, 215729502, 215729482, 215729483, 215729484, 215729485, 215729486, 215729488, 215729489, 215729490, 215729491, 215729493, 215729494 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/215729499/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/215729500/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/215729498/1/ammo.js', 'preload' : true},
+];
